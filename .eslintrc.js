d9f013231f4ef9598e723424eb43c0e47c1f356a
Saulo Matheus de Rezende <saulo_matheprog@hotmail.com>
2024-07-08 02:00:43 -0400

    config prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,8 +13,16 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
+    'plugin:prettier/recommended',
   ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
   rules: {
     'vue/multi-word-component-names': 'off',
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
-}
+};
